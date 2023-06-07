38f6da792a8f0c2f73c51e8904ab8c58835029eb
Kgranv <60527591+Kgranv@users.noreply.github.com>
2023-06-07 18:25:00 +0200

    update mainTest2.py

--- mainTest2.py
+++ mainTest2.py
@@ -2,19 +2,39 @@ import pandas as pd
 import os
 import sys
 import threading
-from datetime import datetime
+import time
 
 outputFileName = "output.log"
 outputFilePath = "./"+outputFileName
 controleFile = "./controle.csv"
 concentrationFile = ""
 concentrationFilePath = "./Data/"
-updateConcentration = False
-timeBetweenChange = 1
-isSuspended = False
-isStop = False
-volumeTotal = 20
-scheduleConcentration = None
+
+class Pompe():
+    debit = 0
+    id = ""
+    
+    def __init__(self,newDebit,newId):
+        self.debit = newDebit
+        self.id = newId
+
+    def injection(self,quantity):
+        print(self.timeToRun(quantity))
+
+    def timeToRun(self,quantity):
+        return int((quantity/self.debit)*60)
+
+class Electrovanne():
+    id = ""
+
+    def __init__(self,id):
+        self.id
+
+    def open(self):
+        print("Ouverture")
+
+    def close(self):
+        print("Fermeture")
 
 class Tee(object):
     def __init__(self, file_name=outputFileName):
@@ -33,6 +53,22 @@ class Tee(object):
         self.file.flush()
         self.stdout.write(message)
 
+
+updateConcentration = False
+isSuspended = False
+isStop = False
+
+timeBetweenChange = 1
+volumeTotal = 20
+startTime = 0
+
+scheduleConcentration = None
+
+debitPompe = [1.0549,1.3314,1.3197]
+listePompe = [Pompe(debitPompe[0],"flow"),Pompe(debitPompe[1],"conc1"),Pompe(debitPompe[2],"conc2")]
+listeVanne = [Electrovanne("purge"),Electrovanne("cells1"),Electrovanne("cells2")]
+
+
 def checkOs():
     """
     Check system OS if windows change path syntax
@@ -49,16 +85,32 @@ def checkOs():
 def getArg():
     global concentrationFilePath
     global concentrationFile
-    if len(sys.argv) != 1:
-        return False
-    else:
+    print(sys.argv)
+    if len(sys.argv) == 2:
         try:
-            #concentrationFile = pd.read_feather(concentrationFilePath+sys.argv[0])
-            concentrationFile = "test"
+            concentrationFile = pd.read_feather(concentrationFilePath+sys.argv[1])
             return True
         except:
+            with Tee():
+                print("Unable to open concentration file")
             return False
-        
+    elif len(sys.argv) == 3:
+        try:
+            concentrationFile = pd.read_feather(concentrationFilePath+sys.argv[1])
+            print("=========Debug mode=========")
+            print(type(concentrationFile["Time"].iloc[86400]))
+            return False
+        except:
+            with Tee():
+                print("Unable to open concentration file")
+            return False
+    elif len(sys.argv) == 1:
+        with Tee():
+            print("Please indicate which data you would use.")
+    else:
+        with Tee():
+            print("Too many argument. I take only 1 argument.")
+        return False
 
 def removeLine():
     """
@@ -104,21 +156,62 @@ def doAction(actionToDo, argument):
     elif  actionToDo == "Stop":
         isStop = True
 
+def getConc():
+    global startTime
+    timeInSecond = int(time.monotonic()-startTime)
+    print("Temps écoulé en seconde : ",timeInSecond)
+    conc1 = concentrationFile["Conc1"].iloc[timeInSecond]
+    conc2 = concentrationFile["Conc2"].iloc[timeInSecond]
+    nutConc = 100.0-conc1-conc2
+    return nutConc,conc1,conc2
 
-def readConcentration():
-    print(volumeTotal)
+def startPurge(purgeTime):
+    for vanne in listeVanne:
+        if vanne.id=="purge":
+            vanne.open()
+        else:
+            vanne.close()
+    time.sleep(purgeTime)
+    for vanne in listeVanne:
+        if vanne.id=="purge":
+            vanne.close()
+        else:
+            vanne.open()
 
 def changeConcentration():
     global timeBetweenChange
     global isSuspended
     global isStop
     global scheduleConcentration
+    global listePompe
 
     if isSuspended:
         if scheduleConcentration is not None and scheduleConcentration.is_alive():
             scheduleConcentration.cancel()
     else:
-        readConcentration()
+        nutConc,conc1,conc2 = getConc()
+        #Appel code calcul injection
+
+        purge = False
+        injectConc1 = 2
+        injectConc2 = 1
+        timePurge = 0
+        if purge:
+            startPurge(timePurge)
+        
+        for vanne in listeVanne:
+            if vanne.id == "cells1" or vanne.id=="cells2":
+                vanne.close()
+
+        for pump in listePompe:
+            if pump.id == "conc1":
+                pump.injection(injectConc1)
+            if pump.id == "conc2":
+                pump.injection(injectConc2)
+
+        for vanne in listeVanne:
+            if vanne.id == "cells1" or vanne.id=="cells2":
+                vanne.open()
 
     if isStop:
         pass
@@ -128,6 +221,7 @@ def changeConcentration():
 
 checkOs()
 if getArg():
+    startTime = time.monotonic()
     changeConcentration()
     while not isStop:
         if getFirstLine() != "":
@@ -143,5 +237,4 @@ if getArg():
     with Tee():
         print("completely stop")
 else:
-    with Tee():
-        print("Unable to open concentration file")
+    pass
